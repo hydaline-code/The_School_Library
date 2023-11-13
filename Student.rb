2bef050185263f921326fe748a268f340af10af8
hydaline-code <djougancharlen@gmail.com>
2023-11-13 16:41:11 +0100

    Add student class

---
+++ Student.rb
@@ -0,0 +1,12 @@
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(classroom = nil, name = "Unknown", age = 0, parent_permission = true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
+end
