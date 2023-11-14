260226ddf2342349704300cbe3805b1db7bd766b
hydaline-code <djougancharlen@gmail.com>
2023-11-15 00:51:21 +0100

    Fix student and teacher

--- student.rb
+++ student.rb
@@ -1,8 +1,10 @@
+require_relative 'person'
+
 class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom: nil, name: 'Unknown', age: 0, parent_permission: true)
-    super(name, age, parent_permission)
+    super(name:name, age:age, parent_permission:parent_permission)
     @classroom = classroom
   end
 
