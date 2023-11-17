f2215ec709cd0c46f3a1713f155641787ba392c3
hydaline-code <djougancharlen@gmail.com>
2023-11-17 12:52:20 +0100

    fix ruby linters errors

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+    super(name:, age:, parent_permission:)
     @specialization = specialization
   end
 
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,11 +2,11 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    output=@nameable.correct_name
-    if  output.length > 10 
-        output = output [0, 10]
-    else 
-      output 
+    output = @nameable.correct_name
+    if output.length > 10
+      output [0, 10]
+    else
+      output
     end
   end
 end
