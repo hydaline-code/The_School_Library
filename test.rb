e284d26b8ecc8733f3dd04d2c2ac801cb0e06822
hydaline-code <djougancharlen@gmail.com>
2023-11-17 12:41:10 +0100

    Add a test file to do some test

---
+++ test.rb
@@ -0,0 +1,14 @@
+
+require_relative 'person'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+# Example usage
+person = Person.new(name: 'maximilianus', age: 22)
+puts "Original Name: #{person.correct_name}"
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts "Capitalized Name: #{capitalized_person.correct_name}"
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
