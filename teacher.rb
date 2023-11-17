3dfad7d7b1f122a4918831f820ad6182235dc4f9
hydaline-code <djougancharlen@gmail.com>
2023-11-17 13:18:33 +0100

    fix ruby linters errors

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
-    super(name , age , parent_permission)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
