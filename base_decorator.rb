369fd9c2d9adb53a53303f971a02bbe765a17d23
hydaline-code <djougancharlen@gmail.com>
2023-11-17 12:40:28 +0100

    Update the base decorator

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,9 +1,11 @@
 require_relative 'nameable'
 
 class BaseDecorator < Nameable
-  def initialize (nameable)
+  def initialize (nameable = Nameable.new())
+    super()
     @nameable = nameable
   end
   def correct_name
     @nameable.correct_name
   end
+end
--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 require_relative 'nameable'
 
+
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
