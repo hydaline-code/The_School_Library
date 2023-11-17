92be4b5a8968f52270e8aff7d2dfe4108c0fedf4
hydaline-code <djougancharlen@gmail.com>
2023-11-17 12:48:01 +0100

    Update the trimmer decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,10 +3,10 @@ require_relative 'base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
     output=@nameable.correct_name
-    if trimmed_output = output.length > 10 
-       trimmed_output =output [0, 10]
+    if  output.length > 10 
+        output = output [0, 10]
     else 
-      output=@nameable.correct_name
+      output 
     end
   end
 end
