aaa2006148e541081bace98d39e876b29b78c66a
hydaline-code <djougancharlen@gmail.com>
2023-11-18 10:31:02 +0100

    Update the testing of the associations

--- test.rb
+++ test.rb
@@ -3,8 +3,10 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 require_relative 'classroom'
 require_relative 'student'
+require_relative 'books'
+require_relative 'rental'
 
-# Example usage
+#  usage
 person = Person.new(name: 'maximilianus', age: 22)
 puts "Original Name: #{person.correct_name}"
 
@@ -15,7 +17,7 @@ capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
 
 
-# Example usage
+#  usage
 classroom = Classroom.new("Room 200")
 classroom2 = Classroom.new("Room 202")
 
@@ -36,4 +38,26 @@ puts student3.classroom.label
 puts student4.classroom.label
 
 puts classroom.students.length 
-puts classroom2.students.length  
+puts classroom2.students.length
+
+# Example usage
+
+# Create persons
+person1 = Person.new(name: 'John', age: 25)
+person2 = Person.new(name: 'Alice', age: 22)
+
+# Create books
+book1 = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+book2 = Book.new('To Kill a Mockingbird', 'Harper Lee')
+
+# Rent books
+rental1 = Rental.new('2023-11-16', person1, book1)
+rental2 = Rental.new('2023-11-17', person2, book2)
+
+# Accessing relationships
+puts "Person1's Rentals: #{person1.rentals.map { |r| r.book.title }}"
+puts "Person2's Rentals: #{person2.rentals.map { |r| r.book.title }}"
+
+puts "Book1's Rentals: #{book1.rentals.map { |r| r.person.name }}"
+puts "Book2's Rentals: #{book2.rentals.map { |r| r.person.name }}"
+
