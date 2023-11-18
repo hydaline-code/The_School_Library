10e939568020fc6513bc766f8b2f838ffd4c4cd4
hydaline-code <djougancharlen@gmail.com>
2023-11-18 10:59:48 +0100

    Adressing issues from linters

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom: nil, name: 'Unknown', age: 0, parent_permission: true)
-    super(name:name, age:age, parent_permission:parent_permission)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
