5ab30f00ab5654328b9e0892280cae4614bcb450
hydaline-code <djougancharlen@gmail.com>
2023-11-18 08:36:36 +0100

    Update students with classroom association

--- student.rb
+++ student.rb
@@ -4,11 +4,16 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom: nil, name: 'Unknown', age: 0, parent_permission: true)
-    super(name, age, parent_permission)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
-
+  
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def add_a_classroom(classroom)
+    @classroom = classroom
+    classroom.adding_student(self)
   end
 end
