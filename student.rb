0cf8357b75dd0ffd3767673ca72e05bccde1213e
hydaline-code <djougancharlen@gmail.com>
2023-12-02 08:55:54 +0100

    Fix linters issues

--- student.rb
+++ student.rb
@@ -3,8 +3,9 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
+    # rubocop:disable Lint/UnusedMethodArgument
   def initialize(classroom: nil, name: 'Unknown', age: 0, parent_permission: true)
-    super(name: name, age: age, id: rand(1000..9999))
+    super(name: name, age: age, parent_permission: parent_permission, id: rand(1000..9999))
     @classroom = classroom
   end
 
